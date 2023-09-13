fadf07d6fc3c2b5fe50e5ce30b8e03edfa19885c
shfiradifa <firaradifa@gmail.com>
2023-09-13 11:00:24 +0700

    meong

--- main/views.py
+++ main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'appName': 'Fir-tix',
+        'appName': 'Tawa Riang Candu',
+        'appDesc': 'Aplikasi pemesanan tiket untuk pertunjukan teater musikal yang mengadaptasi serial film Upin & Ipin, menghadirkan pengalaman tontonan yang penuh tawa dan candu bagi penggemar.',
         'name': 'Shafira Ramadhina Adifa',
         'class': 'PBP-A',
     }
