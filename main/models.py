a24edc9f22fc1dac7cca50d4ef7536128d9b23cb
shfiradifa <firaradifa@gmail.com>
2023-11-22 11:09:40 +0700

    meong

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
-    amount = models.IntegerField()
+    price = models.PositiveIntegerField(default=0)
+    amount = models.PositiveIntegerField(default=0)
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.core import serializers
 
@@ -20,7 +21,6 @@ from django.db.models import Sum
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-...
 @login_required(login_url='/login')
 def show_main(request):
     products = Product.objects.filter(user=request.user)
